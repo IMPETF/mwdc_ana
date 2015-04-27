ca81f57b6072081a564fa650fa02cb722004801a
Yong <yong@impcas.ac.cn>
2015-04-27 10:01:53 +0800

    version for PMO bug fixing

--- scripts/rootlogon.C
+++ scripts/rootlogon.C
@@ -1,5 +1,20 @@
 {
-printf("Load MWDC_ana library\n");
+
+//gSystem->AddIncludePath("-I/home/ufan/workspace/mwdc/formal_test/pre/install/include");
+//gSystem->Load("/home/ufan/workspace/mwdc/formal_test/pre/install/lib/libmwdc.so");
 gSystem->AddIncludePath("-I${CMAKE_INSTALL_PREFIX}/include");
 gSystem->Load("${CMAKE_INSTALL_PREFIX}/lib/libmwdc.so");
+  
+gROOT->ProcessLine(".L draw_multihit.C+");
+gROOT->ProcessLine(".L draw_noise.C+");
+gROOT->ProcessLine(".L draw_mwdctot.C+");
+gROOT->ProcessLine(".L draw_tof.C+");
+gROOT->ProcessLine(".L draw_drift.C+");
+
+gROOT->ProcessLine(".L draw_trajectory.C+");
+gROOT->ProcessLine(".L compare_drift.C+");
+
+gROOT->ProcessLine(".L process.C");
+//
+printf("Load MWDC analysis library!\n");
 }
